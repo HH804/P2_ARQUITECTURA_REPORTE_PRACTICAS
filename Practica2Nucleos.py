41fec23f1ff8aa1e20d07b4e1711604eb6c2ac09
Hernandez Hernandez <a2213520302@alumnos.uat.edu.mx>
2025-09-26 15:59:00 -0600

    Add files via upload

---
+++ Practica2Nucleos.py
@@ -0,0 +1,33 @@
+from machine import Pin
+from time import sleep_ms
+import _thread
+
+verde= Pin(16, Pin.OUT)
+amarillo= Pin(15, Pin.OUT)
+rojo= Pin(14, Pin.OUT)
+
+btn= Pin(20, Pin.IN,Pin.PULL_DOWN)
+azul= Pin(19, Pin.OUT)
+
+leds=(rojo,amarillo,verde)
+
+def estado_pulsador_thread():
+
+    while 1:
+        if btn.value()==1:
+            azul.toggle()
+            sleep_ms(200)
+        
+        else:
+            azul.value(1)
+            sleep_ms(10)
+        
+_thread.start_new_thread(estado_pulsador_thread,())
+
+while 1:
+    for led in leds:
+        led.value(1)
+        sleep_ms(500)
+        led.value(0)
+        sleep_ms(500)
+    
